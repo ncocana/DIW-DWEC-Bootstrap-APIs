1fbeea6a93a818f2c6051eb949c066f034b5e575
ncocana <ncocana@cifpfbmoll.eu>
2024-02-02 18:54:09 +0100

    fix(dragAndDrop): fix bug with dropping pieces and display of winning message

--- src/js/dragAndDrop.js
+++ src/js/dragAndDrop.js
@@ -1,24 +1,17 @@
 let selectedPuzzle = "img1";
 
-// Restart the puzzle
+// Restart the puzzle.
+// Can also change the puzzle images
+// based on the selected option.
 function restartPuzzle() {
-    var imagesDiv = document.getElementById("images");
-    var puzzleDiv = document.getElementById("puzzle");
+    selectedPuzzle = document.getElementById("puzzle-select").value;
+    let imagesDiv = document.getElementById("images");
+    let puzzleDiv = document.getElementById("puzzle");
 
     // Remove existing images
     while (imagesDiv.firstChild) {
         imagesDiv.removeChild(imagesDiv.firstChild);
     }
-
-    // Remove existing images from puzzle div
-    // for (let i = 0; i < 9; i++) {
-    //     var puzzlePiece = document.getElementById("div" + (i + 1));
-
-    //     // Remove existing image
-    //     while (puzzlePiece.firstChild) {
-    //         puzzlePiece.removeChild(puzzlePiece.firstChild);
-    //     }
-    // }
 
     // Remove completed image from puzzle div
     while (puzzleDiv.firstChild) {
@@ -27,7 +20,7 @@ function restartPuzzle() {
 
     // Add empty puzzle pieces back inside puzzle div
     for (let i = 0; i < 9; i++) {
-        var div = document.createElement("div");
+        let div = document.createElement("div");
         div.id = "div" + (i + 1);
         div.ondrop = drop;
         div.ondragover = allowDrop;
@@ -36,22 +29,13 @@ function restartPuzzle() {
 
     // Remove the message of completion if it exists.
     document.getElementById("completion-message").innerHTML = "";
-}
 
-// Function to change the puzzle images based on the selected option
-function changePuzzle() {
-    selectedPuzzle = document.getElementById("puzzle-select").value;
-    var imagesDiv = document.getElementById("images");
-    
-    // Restart the puzzle
-    restartPuzzle();
-
-    // Load new images for the selected puzzle
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            var img = document.createElement("img");
-            img.id = "drag" + (i + (j + 1));
-            img.alt = "drag" + (i + (j + 1));
+    // Load images for the selected puzzle
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let img = document.createElement("img");
+            img.id = "drag" + (i + j * 3 + 1);
+            img.alt = "drag" + (i + j * 3 + 1);
             img.src = "./assets/splitted/" + selectedPuzzle + "/" + i + j + ".jpg";
             img.draggable = true;
             img.ondragstart = drag;
@@ -61,19 +45,19 @@ function changePuzzle() {
 }
 
 function checkPuzzle() {
-    var puzzlePieces = document.querySelectorAll('#puzzle div');
-    var correct = true;
+    let puzzlePieces = document.querySelectorAll('#puzzle div');
+    let correct = true;
 
     puzzlePieces.forEach(function (piece, index) {
-        console.log(piece.firstElementChild.id);
-        console.log(index);
+        // console.log(piece.firstElementChild.id);
+        // console.log(index+1);
         if (piece.firstElementChild.id !== ('drag' + (index + 1))) {
             correct = false;
         }
     });
 
     if (correct) {
-        document.getElementById("completion-message").innerHTML = "Congratulations! You've completed the puzzle!";
+        document.getElementById("completion-message").innerHTML = "<p id='completion-message' class='alert alert-success' role='alert'>Congratulations! You've completed the puzzle!</p>";
         document.getElementById("puzzle").innerHTML = "<img id='img-completed' alt='img-completed' src='./assets/original/" + selectedPuzzle + ".jpg'>"
     }
 }
@@ -88,8 +72,8 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    var targetElement = ev.target;
+    let data = ev.dataTransfer.getData("text");
+    let targetElement = ev.target;
     
     // Check if the target element is an image
     // Otherwise, the puzzle piece is dropped inside the image
